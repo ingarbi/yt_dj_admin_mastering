6fd6f4ef44d00da06157f09f28e8c1904205d1b5
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-06 12:15:09 +0300

    #2 thumbnail

--- book/admin.py
+++ book/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Book, Author
-from django.utils.html import format_html
+
+# from django.utils.html import format_html
+from django.utils.html import mark_safe
 
 admin.site.register(Author)
 
@@ -11,25 +13,32 @@ class BookAdmin(admin.ModelAdmin):
         "id",
         "name",
         "author",
-        "name_colored",
+        # "name_colored",
         "published_date",
-        "thumbnail",
+        "thumbnail_preview",
         "is_available",
     )
-    # exclude = ("thumbnail",)
+    list_display_links = ("id", "name")
     list_filter = ("is_available",)
-    # list_per_page = 2
     search_fields = ("name",)
-    date_hierarchy = "published_date"
-    readonly_fields = ("created_at", "updated_at")
+    readonly_fields = ("thumbnail_preview", "created_at", "updated_at")
 
-    def name_colored(self, obj):
-        if obj.is_available:
-            color_code = "00FF00"
-        else:
-            color_code = "FF0000"
-        html = '<span style="color: #{};">{}</span>'.format(color_code, obj.name)
-        return format_html(html)
+    def thumbnail_preview(self, obj):
+        if obj:
+            return mark_safe('<img src="{}" width="150" height="100" />'.format(obj.thumbnail.url) )
+        return "No photo"
+    thumbnail_preview.short_description = "Photo"
+    # exclude = ("thumbnail",)
+    # list_per_page = 2
+    # date_hierarchy = "published_date"
 
-    name_colored.admin_order_field = "name"
-    name_colored.short_description = "name color"
+    # def name_colored(self, obj):
+    #     if obj.is_available:
+    #         color_code = "00FF00"
+    #     else:
+    #         color_code = "FF0000"
+    #     html = '<span style="color: #{};">{}</span>'.format(color_code, obj.name)
+    #     return format_html(html)
+
+    # name_colored.admin_order_field = "name"
+    # name_colored.short_description = "name color"
--- book/models.py
+++ book/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     published_date = models.DateField()
-    thumbnail = models.ImageField(upload_to='books')
+    thumbnail = models.ImageField(upload_to="books")
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
