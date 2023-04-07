7044e78556387e0e825dc06cd50396b6807ce3ae
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-07 14:29:51 +0300

    #3 SlugField

--- book/admin.py
+++ book/admin.py
@@ -4,8 +4,15 @@ from .models import Book, Author
 # from django.utils.html import format_html
 from django.utils.html import mark_safe
 
-admin.site.register(Author)
 
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+    search_help_text = "search by name here"
+    ordering = ("id",)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -22,15 +29,22 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("is_available",)
     search_fields = ("name",)
     readonly_fields = ("thumbnail_preview", "created_at", "updated_at")
+    autocomplete_fields = ("author",)
+    ordering = ("id",)
 
     def thumbnail_preview(self, obj):
         if obj:
-            return mark_safe('<img src="{}" width="150" height="100" />'.format(obj.thumbnail.url) )
+            return mark_safe(
+                '<img src="{}" width="150" height="100" />'.format(obj.thumbnail.url)
+            )
         return "No photo"
+
     thumbnail_preview.short_description = "Photo"
+
     # exclude = ("thumbnail",)
     # list_per_page = 2
     # date_hierarchy = "published_date"
+    # raw_id_fields = ("author",)
 
     # def name_colored(self, obj):
     #     if obj.is_available:
--- book/models.py
+++ book/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -12,7 +13,7 @@ class Book(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     published_date = models.DateField()
-    thumbnail = models.ImageField(upload_to="books")
+    thumbnail = models.ImageField(upload_to="books", null=True, blank=True)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
